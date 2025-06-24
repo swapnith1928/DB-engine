da2b093e433e3c07f193bed975aaa544af67b294
swapnith1928 <saiswapnith2@gmail.com>
2025-06-24 19:57:42 +0530

    Update storage.cpp

--- src/backend/storage/storage.cpp
+++ src/backend/storage/storage.cpp
@@ -1,243 +1,253 @@
-#include "exec.hpp"
-#include "../storage/storage.hpp"
-#include <iostream>
+#include "storage.hpp"
 #include <fstream>
-#include "director_utils.hpp"
-using namespace std;
+#include <sstream>
+#include <algorithm>
+#include <iostream>
 
 #define FAIL    "\e[0;31m"
 #define SUCCESS "\e[0;32m"
 #define DEFAULT "\e[0;37m"
 
-StorageEngine engine;
-string currentDB = "";
+using namespace std;
 
-string getTableFilePath(const string& table) {
-    if (currentDB == "") return "";
-    return "databases/" + currentDB + "/" + table;
+int StorageEngine::getColumnIndex(const string& table, const string& colName) {
+    ifstream schema(table + ".schema");
+    if(!schema.is_open()){
+        return -2;
+    }
+    string line;
+    int index = 0;
+
+    while (getline(schema, line)) {
+        stringstream ss(line);
+        string type, name;
+        ss >> type >> name;
+        if (name == colName)
+            return index;
+        index++;
+    }
+    return -1; // not found
 }
 
-EXEC_STATUS execute(AST_NODE* node) {
-    switch (node->NODE_TYPE) {
-        case NODE_CREATE_TABLE: {
-            TableSchema schema;
-            schema.name = getTableFilePath(*node->PAYLOAD);
-            if (schema.name == "") {
-                cout <<FAIL<<"[!] ERROR : SELECT A DATABASE OR CREATE A NEW ONE\n"<<DEFAULT;
-                return EXEC_FAIL;
-                break;
-            }
-            for (auto* col : node->CHILDREN) {
-                schema.columns.push_back({*col->PAYLOAD, (col->NODE_TYPE == NODE_INTEGER ? "INT" : "STRING")});
-            }
-            bool status=engine.createTable(schema);
-            if(!status){
-                return EXEC_FAIL;
-            }
-            cout << "Table created.\n";
-            return EXEC_SUCCESS;
-            break;
-        }
-        case NODE_INSERT: {
-            if(currentDB==""){
-                cout <<FAIL<<"[!] ERROR : SELECT A DATABASE OR CREATE A NEW ONE\n"<<DEFAULT;
-                return EXEC_FAIL;
-                break;
-            }
+bool checkValidCond(const string& table,int colIndex,const string& valueType){
+    ifstream schema(table+".schema");
+    string TYPE,NAME;
+    for(int i=0;i<=colIndex;i++){
+        schema>>TYPE>>NAME;
+    }
+    //schema.close();
+    if(TYPE!=valueType){
+        cout<<FAIL<<"[!] ERROR : Mismatch in datatypes of the above condition.\n"<<DEFAULT;
+        return false;
+    }
+    return true;
+}
 
-            vector<string> row;
-            vector<string> type;
-            for (auto* val : node->CHILDREN) {
-                row.push_back(*val->PAYLOAD);
-                if(val->NODE_TYPE==NODE_INTEGER){
-                    type.push_back("INT");
-                }
-                else if(val->NODE_TYPE==NODE_STRING){
-                    type.push_back("STRING");
-                }
-            }
-            bool status = engine.insertRecord(getTableFilePath(*node->PAYLOAD), row,type);
-            if(!status){
-                return EXEC_FAIL;
-            }
-            cout << "Record inserted.\n";
-            return EXEC_SUCCESS;
-            break;
-        }
-        case NODE_SELECT: {
+bool StorageEngine::createTable(const TableSchema& schema) {
+    string schemaPath=schema.name+".schema";
+    string tablePath=schema.name+".txt";
+    ifstream check(schemaPath);
+    if(check.is_open()){
+        cerr<<FAIL<<"[!] ERROR : TABLE '"<<schema.name<<"' already exists.\n"<<DEFAULT;
+        return false;
+    }
+    
+    ofstream out(schemaPath);
+    for (const auto& col : schema.columns) {
+        out << col.type << " " << col.name << "\n";
+    }
+    out.close();
 
-            if(currentDB==""){
-                cout <<FAIL<<"[!] ERROR : SELECT A DATABASE OR CREATE A NEW ONE\n"<<DEFAULT;
-                return EXEC_FAIL;
-                break;
-            }
+    ofstream tableFile(schema.name + ".txt");
+    tableFile.close();
+    return true;
+}
 
-            AST_NODE* cond = node->CHILDREN[0];
-            string op = (cond->NODE_TYPE == NODE_CONDITION_GREATER_THAN ? ">" :
-                         cond->NODE_TYPE == NODE_CONDITION_LESS_THAN ? "<" : "=");
+bool StorageEngine::insertRecord(const string& table, const vector<string>& record,const vector<string>& ltype) {
+    ifstream schemaFile(table+".schema");
+    if(!schemaFile.is_open()){
+        cerr<<FAIL<<"[!] ERROR: could not open schema for table '"<<table<<"'.\n"<<DEFAULT;
+        return false;
+    }
 
-            string tableName = getTableFilePath(*node->PAYLOAD);
-            string columnName = *cond->PAYLOAD;
-            int colIndex = engine.getColumnIndex(tableName, columnName);
+    vector<Column> columns;
+    string type,name;
+    while(schemaFile>>type>>name){
+        columns.push_back({name,type});
+    }
+    schemaFile.close();
 
-            if (colIndex == -1) {
-                cout << FAIL<<"[!] ERROR : Column '" << columnName << "' not found.\n"<<DEFAULT;
-                return EXEC_FAIL;
-                break;
-            }
-            else if(colIndex==-2){
-                cout<<FAIL<<"[!] ERROR : Table '"<<tableName<<"'does not exist.\n"<<DEFAULT;
-                return EXEC_FAIL;
-                break;
-            }
-            if(!checkValidCond(tableName,colIndex,cond->SUB_PAY_LOAD_TYPE)){
-                return EXEC_FAIL;
-            }
-            auto results = engine.selectWhere(tableName, colIndex, op, *cond->SUB_PAY_LOAD);
-            for (auto& row : results) {
-                for (auto& col : row) cout << col << " ";
-                cout << "\n";
-            }
-            return EXEC_SUCCESS;
-            break;
-        }
-        case NODE_DELETE: {
-
-            if(currentDB==""){
-                cout <<FAIL<<"[!] ERROR : SELECT A DATABASE OR CREATE A NEW ONE\n"<<DEFAULT;
-                return EXEC_FAIL;
-                break;
-            }
-
-            AST_NODE* cond = node->CHILDREN[0];
-            string op = (cond->NODE_TYPE == NODE_CONDITION_GREATER_THAN ? ">" :
-                         cond->NODE_TYPE == NODE_CONDITION_LESS_THAN ? "<" : "=");
-
-            string tableName = getTableFilePath(*node->PAYLOAD);
-            string columnName = *cond->PAYLOAD;
-            int colIndex = engine.getColumnIndex(tableName, columnName);
-            if (colIndex == -1) {
-                cout <<FAIL<< "[!] ERROR : Column '" << columnName << "' not found.\n"<<DEFAULT;
-                return EXEC_FAIL;
-                break;
-            }
-            else if(colIndex==-2){
-                cout<<FAIL<<"[!] ERROR : Table '"<<tableName<<"'does not exist.\n"<<DEFAULT;
-                return EXEC_FAIL;
-                break;
-            }
-            if(!checkValidCond(tableName,colIndex,cond->SUB_PAY_LOAD_TYPE)){
-                return EXEC_FAIL;
-            }
-            engine.deleteWhere(tableName, colIndex, op, *cond->SUB_PAY_LOAD);
-            cout << "Matching rows deleted.\n";
-            return EXEC_SUCCESS;
-            break;
-        }
-        case NODE_UPDATE: {
-
-            if(currentDB==""){
-                cout <<FAIL<<"[!] ERROR : SELECT A DATABASE OR CREATE A NEW ONE\n"<<DEFAULT;
-                return EXEC_FAIL;
-                break;
-            }
-
-            AST_NODE* cond = node->CHILDREN[0];
-            string op = (cond->NODE_TYPE == NODE_CONDITION_GREATER_THAN ? ">" :
-                         cond->NODE_TYPE == NODE_CONDITION_LESS_THAN ? "<" : "=");
-
-            string tableName = getTableFilePath(*node->PAYLOAD);
-            string columnName = *cond->PAYLOAD;
-            int colIndex = engine.getColumnIndex(tableName, columnName);
-            if (colIndex == -1) {
-                cout << FAIL<<"[!] ERROR : Column '" << columnName << "' not found.\n"<<DEFAULT;
-                return EXEC_FAIL;
-                break;
-            }
-            
-            ifstream schemaFile(tableName+".schema");
-            if(!schemaFile.is_open()){
-                return EXEC_FAIL;
-            }
-            vector<string> colOrder;
-            string Type,name;
-            while(schemaFile>>Type>>name){
-                colOrder.push_back(name);
-            }
-            schemaFile.close();
-
-            vector<string> newRow(colOrder.size(),"");
-            vector<string> type(colOrder.size(),"");
-            for(AST_NODE* assign:node->UPDATE_CHILDREN){
-                string targetCol=*assign->PAYLOAD;
-                int updateIndex=-1;
-                for(size_t i=0;i<colOrder.size();i++){
-                    if(colOrder[i]==targetCol){
-                        updateIndex=static_cast<int>(i);
-                        break;
-                    }
-                }
-                if(updateIndex==-1){
-                    cerr<<FAIL<<"[!] ERROR : Column '"<<targetCol<<"' does not exist.\n"<<endl<<DEFAULT;
-                    return EXEC_FAIL;
-                }
-                newRow[updateIndex]=*assign->CHILDREN[0]->PAYLOAD;
-                type[updateIndex]=assign->CHILDREN[0]->NODE_TYPE==NODE_INTEGER? "INT" : "STRING";
-            }
-            if(!checkValidCond(tableName,colIndex,cond->SUB_PAY_LOAD_TYPE)){
-                return EXEC_FAIL;
-            }
-            if(!engine.updateWhere(tableName,colIndex,op,*cond->SUB_PAY_LOAD,newRow,type)){
-                return EXEC_FAIL;
-            }
-            cout << "Matching rows updated.\n";
-            return EXEC_SUCCESS;
-        }
-        case NODE_CREATE_DATABASE: {
-            string dbName = *node->PAYLOAD;
-            string dbRoot = "databases";
-            string dbPath = dbRoot+"/"+dbName;
-
-            if(!createDirectoryIfNotExists(dbRoot)){
-                cout<<FAIL<<"[!] ERROR : COULD NOT CREATE 'databases' FOLDER.\n "<<DEFAULT;
-                return EXEC_FAIL;
-            }
-
-            if(directoryExists(dbPath)){
-                cout<<FAIL<<"[!] ERROR : Database '"<<dbName <<"' already exists.\n"<<DEFAULT;
-                return EXEC_FAIL;
-            }
-
-            if(!createDirectoryIfNotExists(dbPath)){
-                cout<<FAIL<<"[!] ERROR : COULD NOT CREATE Database folder\n"<<DEFAULT;
-                return EXEC_FAIL;
-            }
-            cout<<"Database '"<<dbName<<"' created.\n";
-            return EXEC_SUCCESS;
-        }
-        case NODE_USE: {
-            string dbName = *node->PAYLOAD;
-            string dbRoot = "databases";
-            string dbPath = dbRoot+"/"+dbName;
-
-            if(!directoryExists(dbPath)){
-                cout<<FAIL<<"[!] ERROR: Database '"<<dbName<<"' does not exist."<<endl<<DEFAULT;
-                return EXEC_FAIL;
-            }
-
-            currentDB = dbName;
-            cout << "Switched to database '" << currentDB << "'.\n";
-            return EXEC_SUCCESS;
-            break;
-        }
-        case NODE_EXIT: {
-            cout << "Exiting...\n";
-            exit(0);
-            return EXEC_SUCCESS;
-        }
-        default: {
-            cout << "Unrecognized operation.\n";
-            return EXEC_FAIL;
+    if(record.size()!=columns.size()){
+        cerr<<FAIL<<"[!] ERROR: Expected "<<columns.size()<<" values, but got "<<record.size()<<" value.\n"<<DEFAULT;
+        return false;
+    }
+    
+    for(size_t i=0;i<record.size();i++){
+        const string &val=record[i];
+        const string&ctype=columns[i].type;
+        const string &rtype=ltype[i];
+        if(ctype!=rtype){
+                cerr <<FAIL<< "[!] ERROR: Incorrect Datatypes\n"<<DEFAULT;
+                return false;
         }
     }
+    ifstream in(table + ".txt");
+    vector<vector<string>> records;
+    string line;
+
+    while (getline(in, line)) {
+        stringstream ss(line);
+        string val;
+        vector<string> row;
+        while (getline(ss, val, ',')) row.push_back(val);
+        records.push_back(row);
+    }
+    in.close();
+
+    for(const auto& row: records){
+        if(!row.empty()&&row[0]==record[0]){
+            cerr <<FAIL<< "[!] ERROR: Duplicate primary-key value '" << record[0]<< "' in column '" << columns[0].name << "'.\n"<<DEFAULT;
+            return false;
+        }
+    }
+
+    records.push_back(record);
+
+    ofstream out(table + ".txt", ios::trunc);
+    for (const auto& r : records) {
+        for (size_t i = 0; i < r.size(); ++i) {
+            out << r[i];
+            if (i != r.size() - 1) out << ",";
+        }
+        out << "\n";
+    }
+    out.close();
+    return true;
+}
+
+vector<vector<string>> StorageEngine::selectWhere(const string& table, int colIndex, const string& op, const string& value) {
+    ifstream in(table + ".txt");
+
+    string line;
+    vector<vector<string>> result;
+
+    while (getline(in, line)) {
+        stringstream ss(line);
+        string val;
+        vector<string> row;
+        while (getline(ss, val, ',')) row.push_back(val);
+
+        if (colIndex < row.size()) {
+            if ((op == ">" && row[colIndex] > value) ||
+                (op == "<" && row[colIndex] < value) ||
+                (op == "=" && row[colIndex] == value)) {
+                result.push_back(row);
+            }
+        }
+    }
+    return result;
+}
+
+bool StorageEngine::deleteWhere(const string& table, int colIndex, const string& op, const string& value) {
+    ifstream in(table + ".txt");
+    if(!in.is_open()){
+        cerr<<FAIL<<"[!] ERROR: Table '"<<table<<"'does not exist.\n"<<DEFAULT;
+        return false;
+    }
+    vector<vector<string>> records;
+    string line;
+
+    while (getline(in, line)) {
+        stringstream ss(line);
+        string val;
+        vector<string> row;
+        while (getline(ss, val, ',')) row.push_back(val);
+        records.push_back(row);
+    }
+    in.close();
+
+    ofstream out(table + ".txt", ios::trunc);
+    for (const auto& row : records) {
+        if (!((op == ">" && row[colIndex] > value) ||
+              (op == "<" && row[colIndex] < value) ||
+              (op == "=" && row[colIndex] == value))) {
+            for (size_t i = 0; i < row.size(); ++i) {
+                out << row[i];
+                if (i != row.size() - 1) out << ",";
+            }
+            out << "\n";
+        }
+    }
+    out.close();
+    return true;
+}
+
+bool StorageEngine::updateWhere(const string& table,int colIndex,const string& op,const string& value,const vector<string>& newRecord,vector<string>& type){
+
+    ifstream in(table + ".txt");
+    if(!in.is_open()){
+        cerr<<FAIL<<"[!] ERROR: Table '"<<table<<"'does not exist.\n"<<DEFAULT;
+        return false;
+    }
+    vector<vector<string>> records;
+    string line;
+
+    // Read all records
+    while (getline(in, line)) {
+        stringstream ss(line);
+        string val;
+        vector<string> row;
+        while (getline(ss, val, ',')) row.push_back(val);
+        records.push_back(row);
+    }
+    in.close();
+
+
+    ifstream schema(table+".schema");
+    vector<string> COL_TYPE;
+    string TYPE,NAME;
+    while(schema>>TYPE>>NAME){
+        COL_TYPE.push_back(TYPE);
+    }
+    schema.close();
+
+    // Apply updates
+    vector<vector<string>> updatedRecords;
+    for (const auto& row : records) {
+        if (colIndex < row.size()) {
+            bool match = false;
+            if (op == ">" && row[colIndex] > value) match = true;
+            else if (op == "<" && row[colIndex] < value) match = true;
+            else if (op == "=" && row[colIndex] == value) match = true;
+
+            if (match) {
+                vector<string> updatedRow = row;
+                for (size_t i = 0; i < newRecord.size(); ++i) {
+                    if (!newRecord[i].empty()) {
+                        if(type[i]!=COL_TYPE[i]){
+                            cerr<<FAIL<<"[!] ERROR : INVALID TYPE,EXPECTED '"<<COL_TYPE[i]<<"' FOUND '"<<type[i]<<"'."<<endl<<DEFAULT;
+                            return false;
+                        }
+                        updatedRow[i] = newRecord[i];
+                    }
+                }
+                updatedRecords.push_back(updatedRow);
+                continue;
+            }
+        }
+        // If not matched, keep row as is
+        updatedRecords.push_back(row);
+    }
+
+    // Write back to file
+    ofstream out(table + ".txt", ios::trunc);
+    for (const auto& row : updatedRecords) {
+        for (size_t i = 0; i < row.size(); ++i) {
+            out << row[i];
+            if (i != row.size() - 1) out << ",";
+        }
+        out << "\n";
+    }
+    out.close();
+    return true;
 }
